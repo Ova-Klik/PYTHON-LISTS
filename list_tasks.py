39e6b2fdd00b0f8a9ec728ccd67bb96c0c4a2020
I-am-Byte <victor.olaomo@outlook.com>
2026-03-15 10:42:12 +0100

    update

---
+++ list_tasks.py
@@ -0,0 +1,18 @@
+import random 
+
+#numbers=[]
+#
+#for index in range(10):
+#    numbers.append(random.randint(1,50))
+#    
+#print (numbers)
+  
+
+#numbers=[]
+#for index in range(10):
+#    numbers+=[random.randint(1,50)]
+#    
+#print (numbers)
+
+numbers=[random.randint(1,50) for index in range(10)]
+print (numbers)
